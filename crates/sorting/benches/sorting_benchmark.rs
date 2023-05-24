684ca88f26140e5a4b746343fe363a62c3c2b8ee
Pablo Hernández <17086478+Hadronomy@users.noreply.github.com>
2023-05-24 20:29:51 +0100

    refactor: rewrite bechmarks

--- crates/sorting/benches/sorting_benchmark.rs
+++ crates/sorting/benches/sorting_benchmark.rs
@@ -1,41 +1,42 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion, Throughput};
+use criterion::{criterion_group, criterion_main, Bencher, Criterion, Throughput};
+use heck::AsSnakeCase;
 use rand::{distributions::Uniform, Rng};
 
 use sorting::algorithm::SortingAlgorithm;
 
-#[macro_export]
 macro_rules! generate_input {
     ($size:expr) => {{
         let mut rng = rand::thread_rng();
-        let range = Uniform::new(-500, 500);
+        let range = Uniform::new(-($size >> 1), ($size >> 1));
         (0..$size).map(|_| rng.sample(&range)).collect::<Vec<i64>>()
     }};
 }
 
+macro_rules! create_bench_function {
+    ($x:ident) => {
+        |b: &mut Bencher, input: &Vec<i64>| {
+            b.iter(|| SortingAlgorithm::$x.sort_mut(&mut input.to_owned()))
+        }
+    };
+}
+
+macro_rules! create_bench {
+    ($group: expr, $sizes: expr, $($fn_name: ident), *) => {
+        for size in $sizes {
+            let input_items = generate_input!(size);
+            $group.throughput(Throughput::Elements(input_items.len() as u64));
+            $($group.bench_with_input(format!("{}_sort", AsSnakeCase(stringify!($fn_name))), &input_items, create_bench_function!($fn_name));)*
+        }
+    };
+}
+
 fn sorting_bechmark(c: &mut Criterion) {
-    let mut sorting_group = c.benchmark_group("Sorting");
-    for input_items in [
-        generate_input!(50),
-        generate_input!(100),
-        generate_input!(500),
-        generate_input!(1_000),
-        generate_input!(10_000),
-        generate_input!(20_000),
-        generate_input!(50_000),
-        generate_input!(70_000),
-        generate_input!(100_000),
-    ] {
-        sorting_group.throughput(Throughput::Elements(input_items.len() as u64));
-        sorting_group.bench_with_input("Selection Sort", &input_items, |b, input| {
-            b.iter(|| SortingAlgorithm::Selection.sort_mut(black_box(&mut input.to_owned())))
-        });
-        sorting_group.bench_with_input("Insertion Sort", &input_items, |b, input| {
-            b.iter(|| SortingAlgorithm::Insertion.sort_mut(black_box(&mut input.to_owned())));
-        });
-        sorting_group.bench_with_input("Merge Sort", &input_items, |b, input| {
-            b.iter(|| SortingAlgorithm::Merge.sort_mut(black_box(&mut input.to_owned())));
-        });
-    }
+    let sizes = vec![
+        50, 100, 500, 1_000, 2_000, 5_000, 10_000, 20_000, 50_000, 70_000, 100_000, 1_000_000,
+    ];
+
+    let mut sorting_group = c.benchmark_group("sorting_bechmark");
+    create_bench!(sorting_group, sizes, Selection, Insertion, Merge);
 }
 
 criterion_group!(benches, sorting_bechmark);
