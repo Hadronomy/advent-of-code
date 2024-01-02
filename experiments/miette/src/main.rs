dea4645932854d7039d6e7a73d4451fcc7aa8a48
Pablo Hernández <17086478+Hadronomy@users.noreply.github.com>
2024-01-02 18:47:45 +0000

    feat: change example

--- experiments/miette/src/main.rs
+++ experiments/miette/src/main.rs
@@ -2,15 +2,22 @@ use miette::{miette, LabeledSpan, Result, Severity};
 
 fn main() -> Result<()> {
     println!("Hello, world!");
-    let x = 1;
-    let y = 2;
-    let source = "(2 + 2".to_string();
+    let source = "fn main() {
+    println!(\"Hello, world!\";
+    hello
+    hello
+    hello
+    hello
+    hello
+    hello
+    hello
+";
     let report = miette!(
         // Those fields are optional
         severity = Severity::Error,
         code = "expected::rparen",
         help = "always close your parens",
-        labels = vec![LabeledSpan::at_offset(5, "here")],
+        labels = vec![LabeledSpan::at_offset(40, "here")],
         url = "https://example.com",
         // Rest of the arguments are passed to `format!`
         // to form diagnostic message
